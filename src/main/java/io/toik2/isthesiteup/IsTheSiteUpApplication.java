2a27b35c68b3dc5755d89efa60ffc1126ca12198
anison99_ <anita.zych9941@gmail.com>
2022-12-02 20:38:50 +0100

    first commit

---
+++ src/main/java/io/toik2/isthesiteup/IsTheSiteUpApplication.java
@@ -0,0 +1,13 @@
+package io.toik2.isthesiteup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsTheSiteUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsTheSiteUpApplication.class, args);
+	}
+
+}
